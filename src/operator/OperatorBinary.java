263542205936338ee31ae5abb5a8e593951abe53
muadznurrahim123 <muadznurrahim@gmail.com>
2020-04-10 00:33:55 -0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,4 +4,13 @@ public class OperatorBinary {
     String nama = "Muadz Nur Rahim";
     int jumlahSks = 24;
     double indeksPresentasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
